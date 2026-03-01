33c63cb47c1739ca4ea8b51fc3b0a12053692c3f
harun <weluv.ww@gmail.com>
2026-03-01 12:53:27 +0900

    initialize stock-lock-benchmark project with Gradle and Spring Boot

---
+++ src/main/java/nuts/commerce/stocklockbenchmark/StockLockBenchmarkApplication.java
@@ -0,0 +1,13 @@
+package nuts.commerce.stocklockbenchmark;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockLockBenchmarkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StockLockBenchmarkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/nuts/commerce/stocklockbenchmark/StockLockBenchmarkApplicationTests.java
@@ -0,0 +1,13 @@
+package nuts.commerce.stocklockbenchmark;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockLockBenchmarkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
